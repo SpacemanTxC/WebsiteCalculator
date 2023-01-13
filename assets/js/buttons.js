565c37efd01f7e0286ac3873f2926a9f1186318d
Tate Capra <tatecapra@outlook.com>
2023-01-12 19:27:49 -0500

    Create basic display and outline of app and added button notes

---
+++ assets/js/buttons.js
@@ -0,0 +1,15 @@
+//Create 0-9 Button Values
+//Input first value 
+//Input Calculator 4 Function to Use 
+//Input Second Value 
+//Equals Buttons to exacute function and display value
+//Clear Funtion to clear display back to zero
+
+//Stored Value of Buttons Clicked
+let button;
+
+//Get the value from the stored item when clicked and add it nee
+// let buttonValue = () => button +=
+
+//Clear Button: Do I need this here or in the app???
+//let clear = () => displayValue = 0;
